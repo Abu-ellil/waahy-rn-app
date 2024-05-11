b3157d0c837621bb0eef52b7d0fdd8a4cd61fa29
Abo Ellil <mr.abuellil@gmail.com>
2024-05-11 04:28:42 +0300

    Routes Start

---
+++ navigation/MainContainer.js
@@ -0,0 +1,59 @@
+import { StyleSheet, Text, View } from 'react-native'
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+// Screens
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
+import Settinges from "./screens/Settinges";
+
+//Screen names
+const homePage = "Home";
+const profile = "Profile";
+const settingsName = "Settings";
+
+const Tab = createBottomTabNavigator();
+
+const MainContainer = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homePage}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homePage) {
+              iconName = focused ? 'home' : 'home-outline';
+
+            } else if (rn === profile) {
+              iconName = focused ? 'list' : 'list-outline';
+
+            } else if (rn === settingsName) {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'grey',
+          tabBarLabelStyle: { paddingBottom: 10, fontSize: 10 },
+          tabBarStyle: { padding: 10, height: 70 }
+        })}>
+
+        <Tab.Screen name={homePage} component={Home} />
+        <Tab.Screen name={profile} component={Profile} />
+        <Tab.Screen name={settingsName} component={Settinges} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainContainer;
+
+const styles = StyleSheet.create({});
---
+++ navigation/screens/Settinges.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Settinges = () => {
+  return (
+    <View>
+      <Text>Settinges</Text>
+    </View>
+  )
+}
+
+export default Settinges
+
+const styles = StyleSheet.create({})
